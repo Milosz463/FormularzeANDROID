7770fb2edac5354cdc383784d90a4809370b0190
Milosz463 <miloszdawiec6@gmail.com>
2024-11-28 08:47:19 +0100

    sigma

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -88,5 +88,73 @@ namespace WpfApp1
                 }
             }
         }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            string liczbaAA=liczbaA.Text;
+            int liczba1;
+            long wynik;
+            if (int.TryParse(liczbaAA, out liczba1))
+            {
+                wynik = 1;
+                for(int i=1;i <=liczba1; i++)
+                {
+                    wynik= wynik * i;
+                }
+                MessageBox.Show(wynik.ToString());
+            }
+            else
+            {
+                MessageBox.Show("To nie jest liczba");
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            string liczba=liczbaA.Text;
+            string liczb = liczbaB.Text;
+            int liczbaPierw;
+            int liczbaDrug;
+            if(int.TryParse(liczba, out liczbaPierw)&& int.TryParse(liczb, out liczbaDrug)){
+                MessageBox.Show(nwd1(liczbaPierw, liczbaDrug).ToString());
+            }
+            else
+            {
+                MessageBox.Show("To nie jest liczba");
+            }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            string liczbaAa = liczbaA.Text;
+            string liczbaBa = liczbaB.Text;
+            int liczbaAA;
+            int liczbaBB;
+            if (int.TryParse(liczbaAa, out liczbaAA) && int.TryParse(liczbaBa, out liczbaBB))
+            {
+                int potega = 1;
+                for(int i = 1; i <= liczbaBB; i++)
+                {
+                    potega = potega * liczbaAA;
+                    MessageBox.Show(potega.ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("To nie jest liczba");
+            }
+        }
+        private int nwd1( int a, int b)
+        {
+            while (b != 0)
+            {
+                int reszta = a % b;
+                a = b;
+                b = reszta;
+            }
+            return a;
+        }
+        
     }
+    
 }
